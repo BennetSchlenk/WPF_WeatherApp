884869a582c4650908d766fd8d31f2304c6399c9
Bennet <bennet@metricminds.com>
2024-04-10 02:32:17 +0200

    Add project files.

---
+++ WPF_WeatherApp/Model/City.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_WeatherApp.Model
+{
+    internal class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
+        public decimal Lat { get; set; }
+        public decimal Lon { get; set; }
+        public string Url { get; set; }
+    }
+}
