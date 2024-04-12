1a0b430322e03c88fbbee166819a4e1acdb7c007
Bennet <bennet@metricminds.com>
2024-04-12 06:09:33 +0200

    Add display country and icon of current city weather

---
+++ WPF_WeatherApp/ViewModel/ValueConverters/IconPathConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPF_WeatherApp.ViewModel.ValueConverters
+{
+    public class IconPathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string path = (string)value;
+            return path.Replace("//cdn.weatherapi.com", "../Assets");
+            //cdn.weatherapi.com/weather/64x64/night/116.png
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
